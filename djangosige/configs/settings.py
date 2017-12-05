bcb8c8a5d630c76b784ab9a86ac8b882bac54a19
william-casa <william.campolina@gmail.com>
2017-12-05 21:31:48 -0200

    STATIC_ROOT = os.path.join(APP_ROOT, 'static')

--- djangosige/configs/settings.py
+++ djangosige/configs/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(APP_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(APP_ROOT, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(APP_ROOT, 'static'),
