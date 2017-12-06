7357f908bbea4fd7ed520d3b59b08296a725e96d
william-casa <william.campolina@gmail.com>
2017-12-06 21:49:56 -0200

    resolvendo problema do collect static

--- djangosige/configs/settings.py
+++ djangosige/configs/settings.py
@@ -124,8 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(APP_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(APP_ROOT, 'static')
+
 
 STATICFILES_DIRS = [
     os.path.join(APP_ROOT, 'static'),
